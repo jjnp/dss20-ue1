e4fccee326fcbf13b921820a836a92c4d8d5e4ae
Jacob Palecek <jacob.palecek@outlook.com>
2020-04-19 14:48:43 +0200

    finish experiment

--- part1/classify.py
+++ part1/classify.py
@@ -1,9 +1,29 @@
+
+from numpy.random import seed
+# Set seed value used by Keras for reproducible results
+seed(42)
+
 import pandas as pd
 import numpy as np
 from keras.preprocessing.text import Tokenizer
 from keras import models
 from keras import layers
 from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+from numpy import zeros
+
+def token_to_vector(sequences, size=10000):
+    results = zeros((len(sequences), size))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1
+    return results
+
+def class_label_to_vector(class_labels):
+    results = zeros(len(class_labels))
+    for i, label in enumerate(class_labels):
+        if (label.lower() == 'spam'):
+            results[i] = 1
+    return results
 
 rawdata = pd.read_csv('spam.csv', encoding='cp1250')
 rawdata = rawdata.dropna(axis='columns')
@@ -18,5 +38,62 @@ tokenizer.fit_on_texts(X_train)
 X_train_tokenized = tokenizer.texts_to_sequences(X_train)
 X_test_tokenized = tokenizer.texts_to_sequences(X_test)
 
+print('Vectorizing tokenized input')
+x_train = token_to_vector(X_train_tokenized, 10000)
+x_test = token_to_vector(X_test_tokenized, 10000)
 
+y_train = class_label_to_vector(y_train)
+y_test = class_label_to_vector(y_test)
+
+train_x_df = pd.DataFrame(x_train)
+print(train_x_df.shape)
+test_x_df = pd.DataFrame(x_test)
+print(test_x_df.shape)
+train_y_df = pd.DataFrame(y_train)
+print(train_y_df.shape)
+test_y_df = pd.DataFrame(y_test)
+print(test_y_df.shape)
+
+
+model = models.Sequential()
+model.add(layers.Dense(4, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(4, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+
+history = model.fit(x_train,y_train,epochs=20,batch_size=100,validation_split=0.5)
+
+
+# visualisation
+epochs=range(1, 21)
+history_dict = history.history
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy Score')
+plt.xlabel('Epochs')
+plt.legend(['training', 'validation'], loc='lower right')
+plt.savefig('accuracy_epoch.png')
+plt.close()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model Loss function')
+plt.ylabel('Loss')
+plt.xlabel('Epochs')
+plt.legend(['training', 'validation'], loc='upper right')
+plt.savefig('loss_epoch.png')
+
+results = model.evaluate(x_test, y_test)
+
+outputfile = open('results.txt', 'w+')
+outputfile.write('Final results after training for 20 Epochs\n')
+outputfile.write('Accuracy: %s\n' % (results[1]))
+outputfile.write('Loss: %s\n' % (results[0]))
+outputfile.flush()
+outputfile.close()
+
+print(model.metrics_names)
+print('Test result: ', results)
 
